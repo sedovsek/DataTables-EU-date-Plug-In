40694133d33828ab24ba0d1e440271b1e399ae26
Robert Sedovšek <robert.sedovsek@gmail.com>
2011-04-18 00:15:21 -0700

    Edited dataTables.dateFormat.js via GitHub

--- dataTables.dateFormat.js
+++ dataTables.dateFormat.js
@@ -1,4 +1,6 @@
 function calculate_date(date) {
+	var date = date.replace(" ", "");
+	
 	if (date.indexOf('.') > 0) {
 		/*date a, format dd.mn.(yyyy) ; (year is optional)*/
 		var eu_date = date.split('.');
@@ -9,19 +11,19 @@ function calculate_date(date) {
 	
 	/*year (optional)*/
 	if (eu_date[2]) {
-		var year = $.trim(eu_date[2]);
+		var year = eu_date[2];
 	} else {
 		var year = 0;
 	}
 	
 	/*month*/
-	var month = $.trim(eu_date[1]);
+	var month = eu_date[1];
 	if (month.length == 1) {
 		month = 0+month;
 	}
 	
 	/*day*/
-	var day = $.trim(eu_date[0]);
+	var day = eu_date[0];
 	if (day.length == 1) {
 		day = 0+day;
 	}
